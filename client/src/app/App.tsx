ec09cb9e2ed8a76dc2a20eae482ba2da8fdc45f1
vulpeso <vulpes.lis@gmail.com>
2022-02-22 23:26:36 +0100

    remove redux

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,15 +1,10 @@
 import { QuizController } from '../trivia/QuizController';
 import PaletteProvider from '../core/PaletteProvider';
-import { Provider } from 'react-redux';
-import { store } from '../core/store';
-
 
 function App() {
   return (
     <PaletteProvider>
-      <Provider store={store}>
-        <QuizController></QuizController>
-      </Provider>
+      <QuizController></QuizController>
     </PaletteProvider>
   );
 }
--- node_modules/client/src/trivia/state/selectors.ts
+++
@@ -1,4 +0,0 @@
-import { TriviaSliceState } from "./slice";
-
-// Other code such as selectors can use the imported `RootState` type
-export const selectElapsedTime = (state: TriviaSliceState) => state.trivia.time.elapsedTime
